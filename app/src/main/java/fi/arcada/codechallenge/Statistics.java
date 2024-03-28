bcb16b7c91368e847593a6af2002439f94835044
Fredrik Welander <welandfr@arcada.fi>
2024-03-28 15:47:44 +0200

    lektion 3

--- app/src/main/java/fi/arcada/codechallenge/Statistics.java
+++ app/src/main/java/fi/arcada/codechallenge/Statistics.java
@@ -1,13 +1,43 @@
 package fi.arcada.codechallenge;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Statistics {
 
-    public static double calcMean(double[] values) {
+    public static double calcMedian(ArrayList<Double> values) {
+
+        // vi skapar en kopia av values
+        ArrayList<Double> sorted = new ArrayList<>(values);
+        // sedan sorterar vi kopian. Annars sorteras ursprungliga datamängden!
+        Collections.sort(sorted);
+
+        // Detta funkar, ännu bättre skulle vara att kolla skilt för jämna datamängder
+        // med medelvärde av de två mittersta
+        int midIndex = sorted.size()/2;
+
+        // medianen är dett mittersta värdet i vår sorterade datamängd!
+        return sorted.get(midIndex);
+
+    }
+
+    public static double calcMean(ArrayList<Double> values) {
         double sum = 0;
-        for (int i = 0; i < values.length; i++) {
-            sum += values[i];
+        for (int i = 0; i < values.size(); i++) {
+            sum += values.get(i);
         }
 
-        return sum / values.length;
+        return sum / values.size();
+    }
+
+    public static double calcStdev(ArrayList<Double> values) {
+        double mean = calcMean(values);
+        double sumDeviation = 0;
+        for (int i = 0; i < values.size(); i++) {
+            sumDeviation += Math.pow(values.get(i)-mean, 2);
+        }
+
+        return Math.sqrt(sumDeviation / values.size());
+
     }
 }
